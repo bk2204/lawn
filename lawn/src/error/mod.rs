c009123c233315a81b679c11e0e8cf66adaa3a27
brian m. carlson <sandals@crustytoothpaste.net>
2023-07-14 22:35:52 +0000

    Make WrongTypeError return the original error
    When we use `TryFrom`, we consume the original object.  If our error is
    not the kind we wanted it to be, it would be nice to be able to try to
    convert it to a _different_ type.  To do that, make our `WrongTypeError`
    classes return the original error.

--- lawn/src/error/mod.rs
+++ lawn/src/error/mod.rs
@@ -113,8 +113,8 @@ impl From<Error> for i32 {
     }
 }
 
-#[derive(Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
-pub struct WrongTypeError;
+#[derive(Debug)]
+pub struct WrongTypeError(pub Error);
 
 impl TryFrom<Error> for handler::Error {
     type Error = WrongTypeError;
@@ -123,11 +123,15 @@ impl TryFrom<Error> for handler::Error {
             ErrorKind::HandlerError => match e.cause {
                 Some(mut cause) => match cause.downcast_mut::<handler::Error>() {
                     Some(e) => Ok(std::mem::replace(e, handler::Error::Unserializable)),
-                    None => Err(WrongTypeError),
+                    None => Err(WrongTypeError(Error {
+                        kind: e.kind,
+                        cause: Some(cause),
+                        message: e.message,
+                    })),
                 },
-                None => Err(WrongTypeError),
+                None => Err(WrongTypeError(e)),
             },
-            _ => Err(WrongTypeError),
+            _ => Err(WrongTypeError(e)),
         }
     }
 }
@@ -139,7 +143,7 @@ impl TryFrom<Error> for protocol::Error {
         if let handler::Error::ProtocolError(e) = err {
             return Ok(e);
         }
-        Err(WrongTypeError)
+        Err(WrongTypeError(err.into()))
     }
 }
 
@@ -147,7 +151,8 @@ impl TryFrom<Error> for io::Error {
     type Error = WrongTypeError;
     fn try_from(e: Error) -> Result<io::Error, Self::Error> {
         let err = protocol::Error::try_from(e)?;
-        err.try_into().map_err(|_| WrongTypeError)
+        let e: Result<io::Error, _> = err.try_into();
+        e.map_err(|e| WrongTypeError(handler::Error::from(e.0).into()))
     }
 }
 
